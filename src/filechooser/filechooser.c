282ce0bc7388cbe31c129d52363d17d52fa5c10c
Huy Hoang <boydaihungst@gmail.com>
2025-07-05 15:57:42 +0700

    refactor: abort log and return code

--- src/filechooser/filechooser.c
+++ src/filechooser/filechooser.c
@@ -106,8 +106,8 @@ static int exec_filechooser(void *data, bool writing, bool multiple,
 
   FILE *fp = fopen(PATH_PORTAL, "r+");
   if (fp == NULL) {
-    logprint(ERROR, "failed to open " PATH_PORTAL);
-    return -1;
+    logprint(DEBUG, "Aborted");
+    return 0;
   }
 
   size_t num_lines = 0;
